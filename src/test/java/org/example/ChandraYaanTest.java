4b888f57cc1be7bf5cde7b6e5809deca361434ed
Gupta, Yogita <05yogitagupta@gmail.com>
2023-08-18 00:32:06 +0530

    Add RotateRight test of chandrayaan

--- src/test/java/org/example/ChandraYaanTest.java
+++ src/test/java/org/example/ChandraYaanTest.java
@@ -301,6 +301,48 @@ public class ChandraYaanTest {
         assertEquals(0, yaan.getZ());
     }
 
+    @Test
+    @DisplayName("SpaceCraft should Rotate Right in West Direction")
+    void spaceCraftShouldRotateRightInWestDirection(){
+        ChandraYaan yaan = new ChandraYaan(0, 0, 0, Direction.West);
+        yaan.rotateRight(); // RotateRight
+        assertEquals(Direction.North, yaan.getDirection());
+        assertEquals(0, yaan.getY());
+        assertEquals(0, yaan.getX());
+        assertEquals(0, yaan.getZ());
+    }
 
+    @Test
+    @DisplayName("SpaceCraft should Rotate Right in East Direction")
+    void spaceCraftShouldRotateRightInEastDirection(){
+        ChandraYaan yaan = new ChandraYaan(0, 0, 0, Direction.East);
+        yaan.rotateRight(); // RotateRight
+        assertEquals(Direction.South, yaan.getDirection());
+        assertEquals(0, yaan.getY());
+        assertEquals(0, yaan.getX());
+        assertEquals(0, yaan.getZ());
+    }
+
+    @Test
+    @DisplayName("SpaceCraft should Rotate Right in North Direction")
+    void spaceCraftShouldRotateRightInNorthDirection(){
+        ChandraYaan yaan = new ChandraYaan(0, 0, 0, Direction.North);
+        yaan.rotateRight(); // RotateRight
+        assertEquals(Direction.East, yaan.getDirection());
+        assertEquals(0, yaan.getY());
+        assertEquals(0, yaan.getX());
+        assertEquals(0, yaan.getZ());
+    }
+
+    @Test
+    @DisplayName("SpaceCraft should Rotate Right in South Direction")
+    void spaceCraftShouldRotateRighttInSouthDirection(){
+        ChandraYaan yaan = new ChandraYaan(0, 0, 0, Direction.South);
+        yaan.rotateRight(); // RotateRight
+        assertEquals(Direction.West, yaan.getDirection());
+        assertEquals(0, yaan.getY());
+        assertEquals(0, yaan.getX());
+        assertEquals(0, yaan.getZ());
+    }
 
 }
